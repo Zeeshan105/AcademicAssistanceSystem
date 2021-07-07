7c027879962609ae7854974f6a227ffdaea8920a
HUAYI <48390096+ChenHuayi131@users.noreply.github.com>
2021-07-07 14:11:30 +0800

    Fill some content of DataAccessObject and rename QC.script to QQ.script. Then insert dependencies in .script file

--- app/src/main/java/comp3350/AAS/database/DataAccessObject.java
+++ app/src/main/java/comp3350/AAS/database/DataAccessObject.java
@@ -18,14 +18,22 @@ public class DataAccessObject implements DataAccess {
     private String dbName;
     private String dbType;
 
-    private ArrayList<Quiz> students;
-    private ArrayList<Question> courses;
-    private ArrayList<FlashCard> scs;
+//    private ArrayList<Quiz> students;
+//    private ArrayList<Question> courses;
+//    private ArrayList<FlashCard> scs;
 
     private String cmdString;
     private int updateCount;
     private String result;
     private static String EOF = "  ";
+
+    // Quiz
+    private ArrayList<Quiz> quizList=new ArrayList<>();
+    private int completedQuizzes=0;
+
+    // Card
+    private ArrayList<CardFolder> listOfFolder= new ArrayList<>();
+
 
     public DataAccessObject(String dbName) {
         this.dbName = dbName;
@@ -59,49 +67,99 @@ public class DataAccessObject implements DataAccess {
         }
         System.out.println("Closed " + dbType + " database " + dbName);
     }
-    public ArrayList<CardFolder> getFolders(){
-        return new ArrayList<>();
+
+
+    public void addCard(String title, String desc, String folderName){
+        int index = -1; // Place holder index to check if the flashcard belongs to an existing or new flashcard folder.
+
+        for(int i = 0; i < listOfFolder.size();i++){
+            if(listOfFolder.get(i).getFolderName().equals(folderName)){
+                index = i;
+                break;
+            }
+        }
+
+        if(index == -1){
+            CardFolder newFolder = new CardFolder(folderName);
+            newFolder.addCard(title,desc);
+            listOfFolder.add(newFolder);
+        } else {
+            listOfFolder.get(index).addCard(title, desc);
+        }
     }
 
     public ArrayList<String> getFolderNames(){
-        return new ArrayList<>();
+        ArrayList<String> folderNames = new ArrayList<>();
+
+        for (int i = 0; i < listOfFolder.size(); i++) {
+            folderNames.add(listOfFolder.get(i).getFolderName());
+        }
+        return folderNames;
+    }
+
+    public ArrayList<CardFolder> getFolders(){
+        return listOfFolder;
     }
 
     public void deleteFolder(int index){
-
+        listOfFolder.remove(index);
     }
 
-    public void addCard(String title,String desc, String folderName){
-
-    }
 
     public void addQuiz(Question question, String name) {
+        int index=-1; // Place holder index to check if the question belongs to an existing or new quiz.
 
+        for (int i = 0; i < quizList.size(); i++) {
+            Quiz selectedQuiz = quizList.get(i);
+            if (selectedQuiz.getQuizName().compareTo(name)==0) {
+                index=i;
+                break;
+            }
+        }
+
+        if (index==-1) {
+            Quiz quiz = new Quiz(name);
+            quiz.addQuestion(question);
+            quizList.add(quiz);
+        } else {
+            Quiz quiz = quizList.get(index);
+            quiz.addQuestion(question);
+        }
     }
 
     public ArrayList<Quiz> getQuizList() {
-        return null;
+        return quizList;
     }
 
     public ArrayList<String> getAllQuizName() {
-        return null;
+        ArrayList<String> quizNames = new ArrayList<>();
+
+        for (int i = 0; i < quizList.size(); i++) {
+            quizNames.add(quizList.get(i).getQuizName());
+        }
+        return quizNames;
     }
 
     public ArrayList<String> generateQuizGradesList() {
-        return null;
+        ArrayList<String> completedQuizList = new ArrayList<String>();
+        for (int i = 0; i < quizList.size(); i++) {
+            if (quizList.get(i).isComplete()) {
+                completedQuizList.add( quizList.get(i).getQuizName() + "\nMark: " + (int)quizList.get(i).getQuizResult() + "/" + quizList.get(i).getQuestionCount() );
+            }
+        }
+        return completedQuizList;
     }
 
     public int getCompletedQuizzes() {
-        return 0;
+        return completedQuizzes;
     }
 
     public void setCompletedQuizzes(int numCompleted) {
-
+        completedQuizzes = numCompleted;
     }
 
 
-    public String processSQLError(Exception e)
-    {
+    public String processSQLError(Exception e) {
         String result = "*** SQL Error: " + e.getMessage();
 
         // Remember, this will NOT be seen by the user!
