f8db41836a63fa48602fa13ee369a3dc0d517998
HUAYI <48390096+ChenHuayi131@users.noreply.github.com>
2021-07-14 02:00:27 +0800

    Update CardFolder and Quiz test

--- app/src/test/java/comp3350/AAS/tests/object/CardFolderTest.java
+++ app/src/test/java/comp3350/AAS/tests/object/CardFolderTest.java
@@ -1,17 +1,22 @@
 package comp3350.AAS.tests.object;
 
 import junit.framework.TestCase;
+
+import java.util.ArrayList;
+
 import comp3350.AAS.object.CardFolder;
 
 
 public class CardFolderTest extends TestCase {
     private CardFolder cardFolder;
+    private ArrayList<CardFolder>folderArrayList;
 
     public CardFolderTest(String arg0){
         super(arg0);
     }
 
     public void setUp(){
+        folderArrayList=new ArrayList<>();
         cardFolder=new CardFolder("Name");
 
         assertNotNull(cardFolder);
@@ -63,7 +68,7 @@ public class CardFolderTest extends TestCase {
         assertEquals(null, cardFolder.getCardDescription().get(0));
     }
 
-    public void testFolderName(){
+    public void testMultiFolderName(){
         cardFolder=new CardFolder("Folder_One");
         assertNotNull(cardFolder);
         assertEquals("Folder_One", cardFolder.getFolderName());
@@ -79,6 +84,31 @@ public class CardFolderTest extends TestCase {
         cardFolder=new CardFolder(null);
         assertNotNull(cardFolder);
         assertNull(cardFolder.getFolderName());
+    }
+
+    public void testMultiFolderTitleAndDesc(){
+        cardFolder=new CardFolder("Folder_1");
+        folderArrayList.add(cardFolder);
+        cardFolder=new CardFolder("Folder_2");
+        folderArrayList.add(cardFolder);
+
+        folderArrayList.get(0).addCard("Title1", "Description1");
+        assertEquals("Title1", folderArrayList.get(0).getCardList().get(0).getTitle());
+        assertEquals("Description1", folderArrayList.get(0).getCardList().get(0).getDescription());
+
+
+        folderArrayList.get(1).addCard("Title2", "Description2");
+        folderArrayList.get(1).addCard("Title3", "Description3");
+        folderArrayList.get(1).addCard("Title4", "Description4");
+
+        assertEquals("Title2", folderArrayList.get(1).getCardList().get(0).getTitle());
+        assertEquals("Description2", folderArrayList.get(1).getCardList().get(0).getDescription());
+
+        assertEquals("Title3", folderArrayList.get(1).getCardList().get(1).getTitle());
+        assertEquals("Description3", folderArrayList.get(1).getCardList().get(1).getDescription());
+
+        assertEquals("Title4", folderArrayList.get(1).getCardList().get(2).getTitle());
+        assertEquals("Description4", folderArrayList.get(1).getCardList().get(2).getDescription());
     }
 
 
--- app/src/test/java/comp3350/AAS/tests/object/QuizTest.java
+++ app/src/test/java/comp3350/AAS/tests/object/QuizTest.java
@@ -1,6 +1,7 @@
 package comp3350.AAS.tests.object;
 
 import junit.framework.TestCase;
+import java.util.ArrayList;
 import comp3350.AAS.object.Question;
 import comp3350.AAS.object.Quiz;
 
@@ -8,6 +9,7 @@ import comp3350.AAS.object.Quiz;
 public class QuizTest extends TestCase {
     private Question question;
     private Quiz quiz;
+    private ArrayList<Quiz>quizArrayList;
 
     public QuizTest(String arg0){
         super(arg0);
@@ -19,6 +21,8 @@ public class QuizTest extends TestCase {
 
         question=new Question("1+1=", "1", "2", "3", "2");
         assertNotNull(question);
+
+        quizArrayList=new ArrayList<>();
     }
 
     public void testNoQuestion(){
@@ -58,22 +62,72 @@ public class QuizTest extends TestCase {
         assertEquals (99.86, quiz.getQuizResult());
     }
 
-    public void testQuizName(){
+    public void testMultiQuizName(){
         quiz=new Quiz("Science Quiz");
         assertNotNull(quiz);
-        assertEquals("Science Quiz", quiz.getQuizName());
+        quizArrayList.add(quiz);
+        assertEquals("Science Quiz", quizArrayList.get(0).getQuizName());
 
         quiz=new Quiz("1234");
         assertNotNull(quiz);
-        assertEquals("1234", quiz.getQuizName());
+        quizArrayList.add(quiz);
+        assertEquals("1234", quizArrayList.get(1).getQuizName());
 
         quiz=new Quiz("");
         assertNotNull(quiz);
-        assertEquals("", quiz.getQuizName());
+        quizArrayList.add(quiz);
+        assertEquals("", quizArrayList.get(2).getQuizName());
 
         quiz=new Quiz(null);
         assertNotNull(quiz);
-        assertNull(quiz.getQuizName());
+        quizArrayList.add(quiz);
+        assertNull(quizArrayList.get(3).getQuizName());
     }
+
+    public void testMultiQuizQuestion(){
+        quiz=new Quiz("Folder_1");
+        quizArrayList.add(quiz);
+        quiz=new Quiz("Folder_2");
+        quizArrayList.add(quiz);
+
+        question=new Question("Question1", "A", "B", "C", "A");
+        quizArrayList.get(0).addQuestion(question);
+        assertEquals(1, quizArrayList.get(0).getQuestionCount());
+        assertEquals("Question1", quizArrayList.get(0).getQuestionList().get(0).getQuestion());
+        assertEquals("A", quizArrayList.get(0).getQuestionList().get(0).getOption1());
+        assertEquals("B", quizArrayList.get(0).getQuestionList().get(0).getOption2());
+        assertEquals("C", quizArrayList.get(0).getQuestionList().get(0).getOption3());
+        assertEquals("A", quizArrayList.get(0).getQuestionList().get(0).getKey());
+
+
+        question=new Question("Question2", "A", "B", "C", "A");
+        quizArrayList.get(1).addQuestion(question);
+        question=new Question("Question3", "A", "B", "C", "B");
+        quizArrayList.get(1).addQuestion(question);
+        question=new Question("Question4", "A", "B", "C", "C");
+        quizArrayList.get(1).addQuestion(question);
+
+        assertEquals(3, quizArrayList.get(1).getQuestionCount());
+
+        assertEquals("Question2", quizArrayList.get(1).getQuestionList().get(0).getQuestion());
+        assertEquals("A", quizArrayList.get(1).getQuestionList().get(0).getOption1());
+        assertEquals("B", quizArrayList.get(1).getQuestionList().get(0).getOption2());
+        assertEquals("C", quizArrayList.get(1).getQuestionList().get(0).getOption3());
+        assertEquals("A", quizArrayList.get(1).getQuestionList().get(0).getKey());
+
+        assertEquals("Question3", quizArrayList.get(1).getQuestionList().get(1).getQuestion());
+        assertEquals("A", quizArrayList.get(1).getQuestionList().get(1).getOption1());
+        assertEquals("B", quizArrayList.get(1).getQuestionList().get(1).getOption2());
+        assertEquals("C", quizArrayList.get(1).getQuestionList().get(1).getOption3());
+        assertEquals("B", quizArrayList.get(1).getQuestionList().get(1).getKey());
+
+        assertEquals("Question4", quizArrayList.get(1).getQuestionList().get(2).getQuestion());
+        assertEquals("A", quizArrayList.get(1).getQuestionList().get(2).getOption1());
+        assertEquals("B", quizArrayList.get(1).getQuestionList().get(2).getOption2());
+        assertEquals("C", quizArrayList.get(1).getQuestionList().get(2).getOption3());
+        assertEquals("C", quizArrayList.get(1).getQuestionList().get(2).getKey());
+
+    }
+
 
 }
