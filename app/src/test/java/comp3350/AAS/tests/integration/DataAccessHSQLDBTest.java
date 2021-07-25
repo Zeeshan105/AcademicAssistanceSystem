c8e5f525292cf14629f4b0ca2318d9b9e82182da
ChenHuayi131 <chenhuayi2000@gmail.com>
2021-07-25 17:27:50 +0800

    Fix DataAccessObject fix both stub and real db of DataAccessHSQLDBTest

--- app/src/test/java/comp3350/AAS/tests/integration/DataAccessHSQLDBTest.java
+++ app/src/test/java/comp3350/AAS/tests/integration/DataAccessHSQLDBTest.java
@@ -2,6 +2,9 @@ package comp3350.AAS.tests.integration;
 
 import junit.framework.TestCase;
 import comp3350.AAS.application.Main;
+import comp3350.AAS.application.Services;
+import comp3350.AAS.database.DataAccess;
+import comp3350.AAS.tests.database.DataAccessTest;
 
 public class DataAccessHSQLDBTest extends TestCase{
     private static String dbName = Main.dbName;
@@ -10,8 +13,18 @@ public class DataAccessHSQLDBTest extends TestCase{
         super(arg0);
     }
 
-    public void test(){
+    public void testDataAccess(){
+        DataAccess dataAccess;
+        Services.closeDataAccess();
+        System.out.println("\nStarting Integration test DataAccess (using default DB)");
 
+        // Use the following two statements to run with the real database
+        Services.createDataAccess(dbName);
+        dataAccess = Services.getDataAccess(dbName);
+
+        DataAccessTest.dataAccessTest(dataAccess);
+        Services.closeDataAccess();
+        System.out.println("Finished Integration test DataAccess (using default DB)");
     }
 
 }
